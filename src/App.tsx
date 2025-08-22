e416d8d91238da590e3ed8983bc2b74df9107711
YCCC VR Lab <198592267+YCCCVRLab@users.noreply.github.com>
2025-08-22 13:11:11 -0400

    Fix GitHub Pages routing with basename and SPA fallback

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/yccc-nurse-stash">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
