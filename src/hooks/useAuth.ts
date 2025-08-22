c5c00c786c15bcd68205e969092bc6985207100e
YCCC VR Lab <198592267+YCCCVRLab@users.noreply.github.com>
2025-08-22 13:48:52 -0400

    🔧 Fix regex syntax error in password validation

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -47,7 +47,7 @@ const validatePasswordStrength = (password: string): { isValid: boolean; errors:
     errors.push('Password must contain at least one number');
   }
   
-  if (!/[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]/.test(password)) {
+  if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
     errors.push('Password must contain at least one special character');
   }
   
