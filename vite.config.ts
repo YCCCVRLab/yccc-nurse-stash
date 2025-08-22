8fd98f820f63c834fc622dddea9eba2d0abf7820
YCCC VR Lab <198592267+YCCCVRLab@users.noreply.github.com>
2025-08-22 13:02:31 -0400

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/yccc-nurse-stash/',
   server: {
     host: "::",
     port: 8080,
